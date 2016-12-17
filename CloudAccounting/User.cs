c89a3fe3779082fefd50c8d2a9d8be1d152d8008
Mert Gul <mert456274@gmail.com>
2016-12-17 04:09:45 +0300

    Users baslandı

--- CloudAccounting/User.cs
+++ CloudAccounting/User.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json.Linq;
 namespace CloudAccounting
 {
     public class User
     {
+        public string id;
+        public string Name
+        {
+            get { return this.GetCloudValue("Name"); }
+            set { this.SetCloudValue("Name", value); }
+        }
+        public string Surname
+        {
+            get { return this.GetCloudValue("Surname"); }
+            set { this.SetCloudValue("Surname", value); }
+        }
+
+        private string GetCloudValue(string propName)
+        {
+            return Project.Cloud.Value["Users"][this.id][propName].ToString();
+        }
+        private void SetCloudValue(string propName, string value)
+        {
+            Project.Cloud.Value["Users"][this.id][propName] = value;
+        }
+
     }
 }
