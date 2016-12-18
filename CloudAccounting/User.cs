05d471bcc92c051132785befc07bc4f8154e454b
Mert Gul <mert456274@gmail.com>
2016-12-19 02:35:10 +0300

    Username ve Password (Md5 kullanılarak), firebase e bağlandı

--- CloudAccounting/User.cs
+++ CloudAccounting/User.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using System.Security.Cryptography;
 namespace CloudAccounting
 {
     public class User
@@ -19,7 +20,28 @@ namespace CloudAccounting
             get { return this.GetCloudValue("Surname"); }
             set { this.SetCloudValue("Surname", value); }
         }
-
+        public string Username
+        {
+            get { return this.GetCloudValue("Username"); }
+            set { this.SetCloudValue("Username", value); }
+        }
+        public string Password
+        {
+            get { return this.GetCloudValue("Password"); }            
+            set { this.SetCloudValue("Password", Md5password(value)); }
+        }
+        public static string Md5password (string original)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] btr = Encoding.UTF8.GetBytes(original);
+            btr = md5.ComputeHash(btr);
+            StringBuilder sb = new StringBuilder();
+            foreach (byte ba in btr)
+            {
+                sb.Append(ba.ToString("x2").ToLower());
+            }
+            return sb.ToString();
+        }
         private string GetCloudValue(string propName)
         {
             return Project.Cloud.Value["Users"][this.id][propName].ToString();
